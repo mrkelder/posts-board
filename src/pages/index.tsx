809b656a21a2cc4978fb07aa2e5096822f928bd6
Vlad Aheiev <vageev06@gmail.com>
2023-02-21 10:32:16 +0100

    feat: created a basic layout for posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { AppBar, Box, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from '@mui/material';
 import Head from 'next/head';
 
 const TITLE = 'Global Wall';
@@ -13,17 +21,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-        <AppBar position="static">
-          <Box p={2} sx={{ flexGrow: 1 }}>
-            <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
-              {TITLE}
-            </Typography>
-          </Box>
-        </AppBar>
-      </header>
+      <AppBar position="static">
+        <Box p={2} sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+            {TITLE}
+          </Typography>
+        </Box>
+      </AppBar>
 
-      <main></main>
+      <main>
+        <Stack alignItems="center" py={2} spacing={2}>
+          {new Array(10).fill(null).map((_, index) => (
+            <Card
+              key={`post_${index}`}
+              sx={{ minWidth: 320, maxWidth: 380, width: '100%' }}
+            >
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                title="green iguana"
+              />
+              <CardContent>
+                <Typography variant="h6" component="strong" fontWeight="bold">
+                  Card name
+                </Typography>
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                  sequi corrupti accusamus ipsa voluptate enim quia illum
+                  tempore eum impedit.
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Stack>
+      </main>
     </>
   );
 }
