757b6b5c0650c5d8d6368f9ee427ce810f1d80fd
Vlad Aheiev <vageev06@gmail.com>
2023-03-18 18:44:46 -0500

    feat: created registration page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,30 @@
+import { Button, Paper, TextField, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import Head from "next/head";
+import Link from "next/link";
+
+const TITLE = "Signup";
+
+export default function Signup() {
+  return (
+    <>
+      <Head>
+        <title>{TITLE}</title>
+      </Head>
+
+      <Stack justifyContent="center" alignItems="center" height="100vh">
+        <Paper sx={{ p: 2 }} elevation={4}>
+          <Stack gap={2}>
+            <Typography variant="h5" component="h1">
+              {TITLE}
+            </Typography>
+            <TextField label="Username" />
+            <TextField label="Password" />
+            <Button variant="contained">Login</Button>
+            <Link href="/login">Already have an account?</Link>
+          </Stack>
+        </Paper>
+      </Stack>
+    </>
+  );
+}
