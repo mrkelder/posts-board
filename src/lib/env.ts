5747d96f5e5da488424fdd8509830c4d19910b22
Vlad Aheiev <vageev06@gmail.com>
2023-03-30 18:34:22 -0400

    feat: created a unified env file to contain all env variables

---
+++ src/lib/env.ts
@@ -0,0 +1,12 @@
+type EnvFields =
+  | "hostname"
+  | "aws_access_key"
+  | "aws_secret_key"
+  | "cognito_user_pool_id";
+
+export const env: Record<EnvFields, string> = {
+  hostname: process.env.NEXT_PUBLIC_HOSTNAME ?? "http://localhost:3000",
+  aws_access_key: process.env.AWS_ACCESS_KEY ?? "invalid_credential",
+  aws_secret_key: process.env.AWS_SECRET_KEY ?? "invalid_credential",
+  cognito_user_pool_id: process.env.COGNITO_USER_POOL_ID ?? "invalid_user_pool",
+};
