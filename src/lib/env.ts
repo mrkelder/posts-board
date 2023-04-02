88ab62a32ca82a25b3333715917e078f7d34bca8
Vlad Aheiev <vageev06@gmail.com>
2023-04-02 12:42:47 -0400

    feat: added congito client id env variable

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,11 +2,13 @@ type EnvFields =
   | "hostname"
   | "aws_access_key"
   | "aws_secret_key"
-  | "cognito_user_pool_id";
+  | "cognito_user_pool_id"
+  | "congito_client_id";
 
 export const env: Record<EnvFields, string> = {
   hostname: process.env.NEXT_PUBLIC_HOSTNAME ?? "http://localhost:3000",
   aws_access_key: process.env.AWS_ACCESS_KEY ?? "invalid_credential",
   aws_secret_key: process.env.AWS_SECRET_KEY ?? "invalid_credential",
   cognito_user_pool_id: process.env.COGNITO_USER_POOL_ID ?? "invalid_user_pool",
+  congito_client_id: process.env.COGNITO_CLIENT_ID ?? "invalid_client_id",
 };
